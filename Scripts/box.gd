f618efea7797ffdb0f4807c3e5ef1386f1fef0f8
Jordan Barber <jordanb421@outlook.com>
2025-09-13 18:28:53 +0100

    Adding a basic box obstacle/sprite with physics

---
+++ Scripts/box.gd
@@ -0,0 +1,11 @@
+extends CharacterBody2D
+
+# get the grvaity from the scene
+@onready var gravity_val = ProjectSettings.get_setting("physics/2d/default_gravity")
+@onready var gravity_vec = get_parent().get_meta("gravity_vec")
+
+func _physics_process(delta: float) -> void:
+	gravity_vec = get_parent().get_meta("gravity_vec")
+	velocity += gravity_val*gravity_vec*delta
+	
+	move_and_slide()
