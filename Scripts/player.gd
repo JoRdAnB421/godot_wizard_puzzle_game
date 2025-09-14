bbb9b76dc9cfe96c155c076e83e5cf9a8013c260
Jordan Barber <jordanb421@outlook.com>
2025-09-14 14:16:15 +0100

    Fixing rotation of gravity vector and generalising movement controls

--- Scripts/player.gd
+++ Scripts/player.gd
@@ -12,70 +12,38 @@ extends CharacterBody2D
 @onready var gravity_vec = 	get_parent().get_meta("gravity_vec")
 
 func _physics_process(delta: float) -> void:
-	var gravity_vec = 	get_parent().get_meta("gravity_vec")
-	# Add gravity
+	"""Process the physics simulations"""
+	# Get current gravity direction
+	gravity_vec = 	get_parent().get_meta("gravity_vec")
+	var right_vec = gravity_vec.orthogonal()
+	
+	# Add gravity acceleration
 	velocity += gravity_vec*gravity_val*delta
-	#print(gravity_vec)e
 	
 	# Basic movement and jumping
 	if Input.is_action_just_pressed("jump") and is_on_floor():
-		velocity.y = jump_speed
-	#var direction = Input.get_axis("move_left", "move_right")
-	#velocity.x = direction*speed
-
+		velocity += gravity_vec*jump_speed
+		
+	var direction = Input.get_axis("move_left", "move_right")
+	if direction:
+		velocity = velocity.slide(right_vec) + right_vec*direction*speed
+	else:
+		velocity = velocity.slide(right_vec)
 	move_and_slide()
 
 func _process(_delta: float) -> void:
 	if Input.is_action_just_pressed("cast_spell"):
-		new_rotation()
+		rotate_room()
 
 
-func new_rotation() -> void:
+func rotate_room() -> void:
 	"""Rotate the Wizard and then set reset gravity"""
-	rotation += PI/2
-	print(rotation)
-	var rot_matrix = [Vector2(cos(rotation),-sin(rotation)), 
-					  Vector2(sin(rotation),cos(rotation))
+	rotation += PI/2 # Rotates the player sprite
+	var rot_matrix = [Vector2(0,-1), 
+					  Vector2(1,0)
 					 ]
 	var new_gravity_vec = Vector2(gravity_vec.dot(rot_matrix[0]),
 							  	  gravity_vec.dot(rot_matrix[1])
-								 )
+								 ).round()
 	get_parent().set_meta("gravity_vec", new_gravity_vec)
-
-func rotate_room_about_wizard(clockwise: bool = true) -> void:
-	var wizards_cell = tile_map_layer.local_to_map(
-		tile_map_layer.to_local(position)
-	)
-	
-	# Grabs the cells foor all edges
-	var used_cells = tile_map_layer.get_used_cells()
-	if used_cells.is_empty():
-		return
-		
-	# Store old tiles
-	var old_tiles = []
-	for cell in used_cells:
-		var source_id = tile_map_layer.get_cell_source_id(cell)
-		var atlas_coords = tile_map_layer.get_cell_atlas_coords(cell)
-		old_tiles.append({
-				"cell": cell, 
-				"source_id": source_id,
-				"atlas": atlas_coords})
-	
-	tile_map_layer.clear()
-	
-	# set new tiles
-	for tile in old_tiles:
-		var old_cell = tile["cell"]
-		var rel = old_cell - wizards_cell
-		var rotated = Vector2i(rel.y, -rel.x) # 90 degrees
-		var new_cell = wizards_cell + rotated
-		tile_map_layer.set_cell(new_cell, tile["source_id"], tile["atlas"])
-	
-	
-	# rotate objects inside the room
-	var rel_pos = box.position - position
-	var rot = Vector2(rel_pos.y, -rel_pos.x)
-	var new_pos = box.position + rot
-	
-	box.position = new_pos
+	up_direction = -new_gravity_vec
